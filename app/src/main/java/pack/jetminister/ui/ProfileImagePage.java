33426667432ba342ce35c4406df999d4a6efa176
GhyselsDymas <dg.dymasghysels@gmail.com>
2020-09-03 17:51:56 +0200

    Made a upload page for user profile pic

--- app/src/main/java/pack/jetminister/ui/ProfileImagePage.java
+++ app/src/main/java/pack/jetminister/ui/ProfileImagePage.java
@@ -1,31 +1,77 @@
 package pack.jetminister.ui;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import com.squareup.picasso.Picasso;
 
 import pack.jetminister.R;
 
-public class ProfileImagePage extends Fragment {
+import static android.app.Activity.RESULT_OK;
+
+public class ProfileImagePage  extends AppCompatActivity {
 
 
     public ProfileImagePage() {
         // Required empty public constructor
     }
 
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private Button mButtonChooseImage;
+    private Button mButtonUpload;
+    private Button mButtonCancel;
+    private ImageView mImageView;
+    private ProgressBar mProgressBar;
+    private Uri mImageUri;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_profile_image_page);
+
+        mButtonChooseImage = findViewById(R.id.button_choose_image);
+        mButtonUpload = findViewById(R.id.button_upload);
+        mImageView = findViewById(R.id.image_view);
+        mProgressBar = findViewById(R.id.progress_bar);
+        mButtonChooseImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFileChooser();
+            }
+        });
+        mButtonUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+    }
+
+
+    private void openFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_image_page, container, false);
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null) {
+            mImageUri = data.getData();
+            Picasso.get().load(mImageUri).into(mImageView);
+        }
     }
 }
--- app/src/main/java/pack/jetminister/ui/fragments/ProfileFragment.java
+++ app/src/main/java/pack/jetminister/ui/fragments/ProfileFragment.java
@@ -1,17 +1,26 @@
 package pack.jetminister.ui.fragments;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import pack.jetminister.R;
+import pack.jetminister.ui.LoginOrRegister;
+import pack.jetminister.ui.ProfileImagePage;
 
 public class ProfileFragment extends Fragment {
 
+    private AppCompatActivity mycontext;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -23,9 +32,32 @@ public class ProfileFragment extends Fragment {
     }
 
     @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mycontext = (AppCompatActivity)context;
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+        View rootview =  inflater.inflate(R.layout.fragment_profile, container, false);
+
+        ImageView buttonProfile = rootview.findViewById(R.id.imgTourismHr);
+        buttonProfile.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                openImagePage();
+                return true;
+            }
+        });
+
+        return rootview;
+    }
+
+    private void openImagePage() {
+        Intent intent = new Intent(mycontext , ProfileImagePage.class);
+        startActivity(intent);
     }
 }
