1c31efb2e68ec2d1e60ee01f40f9a975754d9d42
floatingreels <david.gunzburg@gmail.com>
2020-09-18 13:13:40 +0200

    Permissions Activity added

---
+++ app/src/main/java/pack/jetminister/ui/activities/PermissionsActivity.java
@@ -0,0 +1,86 @@
+package pack.jetminister.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.DexterError;
+import com.karumi.dexter.listener.OnDialogButtonClickListener;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.PermissionRequestErrorListener;
+import com.karumi.dexter.listener.multi.CompositeMultiplePermissionsListener;
+import com.karumi.dexter.listener.multi.DialogOnAnyDeniedMultiplePermissionsListener;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.util.List;
+
+import pack.jetminister.R;
+
+public class PermissionsActivity extends AppCompatActivity {
+
+    private static final String TAG = "PermissionsActivity";
+
+    MultiplePermissionsListener permissionsListener = new MultiplePermissionsListener() {
+        @Override
+        public void onPermissionsChecked(MultiplePermissionsReport report) {
+            if (report.areAllPermissionsGranted()) {
+                Intent intent = new Intent(PermissionsActivity.this, BroadcastActivity.class);
+                startActivity(intent);
+                PermissionsActivity.this.finish();
+            } else {
+                Toast.makeText(PermissionsActivity.this, "Not all permissions checked", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        @Override
+        public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken token) {
+            token.continuePermissionRequest();
+        }
+    };
+
+    MultiplePermissionsListener dialogPermissionsListener =
+            DialogOnAnyDeniedMultiplePermissionsListener.Builder
+                    .withContext(this)
+                    .withTitle("Camera and Audio")
+                    .withMessage("You need both Camera and Audio permission to broadcast")
+                    .withButtonText("OK", new OnDialogButtonClickListener() {
+                        @Override
+                        public void onClick() {
+                            askUserPermissions();
+                        }
+                    })
+                    .build();
+
+    MultiplePermissionsListener compositePermissionsListener = new CompositeMultiplePermissionsListener(permissionsListener, dialogPermissionsListener);
+
+    PermissionRequestErrorListener errorListener = new PermissionRequestErrorListener() {
+        @Override public void onError(DexterError error) {
+            Log.e(TAG,  error.toString());
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_permissions);
+        askUserPermissions();
+    }
+
+    private void askUserPermissions() {
+        Dexter.withContext(this)
+                .withPermissions(
+                        Manifest.permission.RECORD_AUDIO,
+                        Manifest.permission.CAMERA)
+                .withListener(compositePermissionsListener)
+                .withErrorListener(errorListener)
+                //.onSameThread() not sure if this is entirely necessary
+                .check();
+    }
+}
