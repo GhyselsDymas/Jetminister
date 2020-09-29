03a7f8a8b78a9be82073925e016531258c3b75a1
floatingreels <david.gunzburg@gmail.com>
2020-09-29 10:02:23 +0200

    Updated Comment object again

--- app/src/main/java/pack/jetminister/data/Comment.java
+++ app/src/main/java/pack/jetminister/data/Comment.java
@@ -2,16 +2,21 @@ package pack.jetminister.data;
 
 public class Comment {
     private String userID;
+    private String username;
     private String body;
 
-    public Comment(String userID,
-                   String body) {
+    public Comment(String userID, String username, String body) {
         this.userID = userID;
+        this.username = username;
         this.body = body;
     }
 
     public String getUserID() {
         return userID;
+    }
+
+    public String getUsername() {
+        return username;
     }
 
     public String getBody() {
@@ -21,6 +26,10 @@ public class Comment {
 
     public void setUserID(String userID) {
         this.userID = userID;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public void setBody(String body) {
